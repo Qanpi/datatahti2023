526dcaa786348d7f87a57922a6aae82be3698927
Qanpi <aleksei.terin@outlook.com>
2022-11-02 23:28:10 +0200

    attempting to change scale of arr values

--- factorial.cpp
+++ factorial.cpp
@@ -23,21 +23,22 @@ int main() {
 
 	int i = 2;
 	int l = 1;
+	int scale = 100;
 	int carry = 0;
 	while(true) {
 		for(int j=0; j<l; j++) {
 			int val = factorial[j] * i;
-			factorial[j] = (val+carry) % 10;
-			carry = (val+carry) / 10;
+			factorial[j] = (val+carry) % scale;
+			carry = (val+carry) / scale;
 		}
 
 		while (carry > 0) {
 			l++;
-			factorial[l-1] = carry % 10;
-			carry /= 10;
+			factorial[l-1] = carry % scale;
+			carry /= scale;
 		}
 
-		if(l == length && check(l)) break;
+		if(l > length/2 && check(l)) break;
 		i++; 
 	}
 	
@@ -49,8 +50,10 @@ bool check(int l) {
 
 	for(int i=0; i<l; i++) {
 		int d = factorial[i];
+		cout << d << " ";
 		count[d]++;
 	}
+	cout << endl;
 
 	for(int i=0; i<10; i++) {
 		if(digits[i] != count[i]) return false;
